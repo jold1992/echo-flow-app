9bf2749cdfe59eb8f23d00ca7d02cf7ab54adea2
john <jold1992@gmail.com>
2023-10-12 22:02:17 -0600

    cc: se agrega un comentario con código relevante

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -75,3 +75,16 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: { providers }
   }
 }
+
+{/*
+  <h1>
+        LOGIN
+      </h1>
+      <div>
+        {Object.values(providers).map((provider) => (
+          <button key={provider.id} onClick={() => signIn(provider.id, {
+            callbackUrl: `${window.location.origin}`
+          })}>Sign in with Google</button>
+        ))}
+      </div>
+ */}
